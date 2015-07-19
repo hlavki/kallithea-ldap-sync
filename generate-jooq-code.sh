72bfef6a2f85b255fa06bb599f0ae5d365906253
Michal Hlavac <miso@hlavki.eu>
2015-07-19 19:29:29 +0200

    use default java path

--- generate-jooq-code.sh
+++ generate-jooq-code.sh
@@ -4,4 +4,4 @@ MAVEN_REPO_PATH=~/.m2/repository
 JOOQ_VERSION=3.2.7
 
 echo $MAVEN_REPO_PATH/org/jooq/jooq/$JOOQ_VERSION/jooq-$JOOQ_VERSION.jar
-/usr/java/default/bin/java -cp $MAVEN_REPO_PATH/org/jooq/jooq/$JOOQ_VERSION/jooq-$JOOQ_VERSION.jar:$MAVEN_REPO_PATH/org/jooq/jooq-meta/$JOOQ_VERSION/jooq-meta-$JOOQ_VERSION.jar:$MAVEN_REPO_PATH/org/jooq/jooq-codegen/$JOOQ_VERSION/jooq-codegen-$JOOQ_VERSION.jar:$MAVEN_REPO_PATH/postgresql/postgresql/8.4-702.jdbc4/postgresql-8.4-702.jdbc4.jar:. org.jooq.util.GenerationTool /jooq-configuration.xml
+java -cp $MAVEN_REPO_PATH/org/jooq/jooq/$JOOQ_VERSION/jooq-$JOOQ_VERSION.jar:$MAVEN_REPO_PATH/org/jooq/jooq-meta/$JOOQ_VERSION/jooq-meta-$JOOQ_VERSION.jar:$MAVEN_REPO_PATH/org/jooq/jooq-codegen/$JOOQ_VERSION/jooq-codegen-$JOOQ_VERSION.jar:$MAVEN_REPO_PATH/postgresql/postgresql/8.4-702.jdbc4/postgresql-8.4-702.jdbc4.jar:. org.jooq.util.GenerationTool /jooq-configuration.xml
